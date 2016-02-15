7620e58b7d11a7d88df08ef4a7f19da3093b39c9
Jonas Almeida <jalmeida@mathbiol.org>
2016-02-15 08:42:54 -0500

    asthma app started

---
+++ app/asthma.js
@@ -0,0 +1,8 @@
+console.log('loading asthma.js')
+
+sbmApps.insertApp({
+    name:"asthma",
+    description:"Experimental asthma app",
+    icon:"https://whatsupatupstate.files.wordpress.com/2012/09/lungs.jpg",
+    onclick:"http://sbu-bmi.github.io/asthma/asthma.js"
+})
