e333745b4ba2420c392cfa60df5bd8ae5251ea18
Jonas Almeida <jalmeida@mathbiol.org>
2016-02-17 14:00:22 -0500

    Create nucleus.js

---
+++ app/nucleus.js
@@ -0,0 +1,8 @@
+console.log('redirecting to https://sbu-bmi.github.io/NucleiMugshot/index.html')
+
+sbmApps.insertApp({
+    name:"nucleus",
+    description:"Mugshot of bad nuclei",
+    icon:"https://sbu-bmi.github.io/NucleiMugshot/icon.jpg",
+    onclick:"https://sbm-it.github.io/apps/app/nucleus_redirect.js"
+})
