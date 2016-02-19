c644805b5b939a97d74e0251e8c4b29653b5f111
Jonas Almeida <jalmeida@mathbiol.org>
2016-02-19 08:54:04 -0500

    Create sodium.js
    private sodium app manifest

---
+++ app/sodium.js
@@ -0,0 +1,8 @@
+// manifest for private App at https://github.com/SBU-BMI/sodium
+
+sbmApps.insertApp({
+    name:"sodium",
+    description:"Cardiology clinical trial on sodium intake",
+    icon:"http://sbu-bmi.github.io/sodium/sodium.png",
+    onclick:"http://sbu-bmi.github.io/sodium/sodium.js"
+})
