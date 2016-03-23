82306eec68dce9831b14bda2007daa17a8274ef4
Jonas Almeida <jalmeida@mathbiol.org>
2016-03-23 11:38:34 -0400

    missing ssl

--- app/fibonacci.js
+++ app/fibonacci.js
@@ -2,5 +2,5 @@ sbmApps.insertApp({
     name:"Fibonacci",
     description:"generates Fibbonatcci series",
     icon:"https://www.mathsisfun.com/numbers/images/fibonacci.jpg",
-    onclick:"http://sbm-it.github.io/fibonacci/index.html"
+    onclick:"https://sbm-it.github.io/fibonacci/index.html"
 })
