f5bf8f202f501c911ef6602f0dda49877ab55a45
Jonas Almeida <jalmeida@mathbiol.org>
2016-03-23 11:36:52 -0400

    Create fibonacci.js

---
+++ app/fibonacci.js
@@ -0,0 +1,6 @@
+sbmApps.insertApp({
+    name:"Fibonacci",
+    description:"generates Fibbonatcci series",
+    icon:"https://www.mathsisfun.com/numbers/images/fibonacci.jpg",
+    onclick:"http://sbm-it.github.io/fibonacci/index.html"
+})
