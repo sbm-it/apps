e12377c19ba204b6e10ba8565df3156de1babdd2
Jonas Almeida <jalmeida@mathbiol.org>
2016-03-23 13:38:32 -0400

    nucleous icon updated

--- app/nucleus.js
+++ app/nucleus.js
@@ -3,6 +3,6 @@ console.log('redirecting to https://sbu-bmi.github.io/NucleiMugshot/index.html')
 sbmApps.insertApp({
     name:"nucleus",
     description:"Mugshot of bad nuclei",
-    icon:"https://sbu-bmi.github.io/NucleiMugshot/icon.jpg",
+    icon:"https://sbu-bmi.github.io/NucleiMugshot/img/icon.jpg",
     onclick:"https://sbm-it.github.io/apps/app/nucleus_redirect.js"
 })
