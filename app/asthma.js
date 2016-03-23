b084298a61718359edd438341ec2549c2a5fcf71
Jonas Almeida <jalmeida@mathbiol.org>
2016-03-23 16:48:12 -0400

    asthma icon update

--- app/asthma.js
+++ app/asthma.js
@@ -3,7 +3,7 @@ console.log('loading asthma.js')
 sbmApps.insertApp({
     name:"asthma",
     description:"Experimental asthma app",
-    icon:"https://whatsupatupstate.files.wordpress.com/2012/09/lungs.jpg",
+    icon:"https://cdn2.iconfinder.com/data/icons/medical-specialties-1/256/Pulmonology-512.png",
     onclick:"https://sbu-bmi.github.io/asthma/index.html"
     //onclick:"https://sbu-bmi.github.io/asthma/asthma.js"
 })
