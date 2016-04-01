27b2656e4a48d290bdb456b750a2af6b26bd19e3
Jonas Almeida <jalmeida@mathbiol.org>
2016-04-01 15:46:35 -0400

    rm dale1

--- app/.getJSON-https/sbm-it.github.io/fibonacci/dale1.js
+++
@@ -1,5 +0,0 @@
-$.getJSON("https://hscvw01.informatics.stonybrook.edu/dev/apps/sbmManifest.json")
- .then(function(x){
-     sbmApps.insertApp(x)
-     console.log('inserted Dale\'s first app, declared with a sbmManifest.json')
-  })
