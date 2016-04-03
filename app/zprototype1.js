5a63175a22127b3fc2cc56d91a07748b03d1b41d
Hasit Shah <shah2129@users.noreply.github.com>
2016-04-03 18:19:54 -0400

    Update zprototype1.js

--- app/zprototype1.js
+++ app/zprototype1.js
@@ -1,4 +1,4 @@
-$.getJSON("http://mongodb-154807.nitrousapp.com:3000/sbmManifest.json#")
+$.getJSON("https://mongodb-154807.nitrousapp.com:3000/sbmManifest.json#")
  .then(function(x){
      sbmApps.insertApp(x)
      console.log('inserted app prototype')
