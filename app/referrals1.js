8f0c6fe01e42ccc5911f23f5ce73696ddad45fab
Wade Jakahi <wjakahi@gmail.com>
2016-04-14 16:42:59 -0400

    Rename
    is it case sensitive?

---
+++ app/referrals1.js
@@ -0,0 +1,6 @@
+console.log('loaded Physician Referrals.js')
+
+// clean up appSpace
+appSpace.innerHTML="loading SBM Physician Referrals"
+
+location.href="https://sbm-it.github.io/referrals/#/referrals-search"
