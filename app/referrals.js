ffdc516cf33b22c107e7980a96e39ed04db9a45a
Wade Jakahi <Wade.jakahi@stonybrook.edu>
2016-04-14 16:53:38 -0400

    Update referrals.js

--- app/referrals.js
+++ app/referrals.js
@@ -1,6 +1,8 @@
 console.log('loaded Physician Referrals.js')
 
-// clean appSpace
-appSpace.innerHTML="loading SBM Physician Referrals"
-
-location.href="https://sbm-it.github.io/referrals/#/referrals-search"
+sbmApps.insertApp({
+    name:"referrals",
+    description:"Physician Referrals",
+    icon:"https://sbm-it.github.io/referrals/img/patientcare-icon.png",
+    onclick:"https://sbm-it.github.io/referrals/#/referrals-search"
+})
