e74cccec67bc5a5eadf991ef9746e1eae138a8b4
Wade Jakahi <wjakahi@gmail.com>
2016-04-20 15:36:31 -0400

    ui

--- app/newinfo.js
+++ app/newinfo.js
@@ -9,9 +9,10 @@ console.log('info.js loaded')
 false||(function(){ // this little trick will have all variables stay local
     var h = ''
     h += '<h3 style="color:maroon">Install It</h3>'
-    h += '<p style="color:navy"><a href="http://sbm-it.github.io/apps/AddAndroidHomeScreen.html"><img width="'+(sbmApps.iconWidth)+'" src="img/android.png"></a>'
-    h += '<a href="http://sbm-it.github.io/apps/AddIOSHomeScreen.html"><img width="'+(sbmApps.iconWidth)+'" src="img/apple.png"></a><br>'
-    h += '<h3 style="color:maroon">Using It</h3>'
+    h += '<p style="color:navy;display:block;margin-left:auto;margin-right:auto">'
+    h += '<a href="http://sbm-it.github.io/apps/AddAndroidHomeScreen.html"><img width="'+(sbmApps.iconWidth)+'" src="img/android.png"></a>'
+    h += '<a href="http://sbm-it.github.io/apps/AddIOSHomeScreen.html"><img width="'+(sbmApps.iconWidth)+'" src="img/apple.png"></a></p>'
+    h += '<h3 style="color:maroon">Use It</h3>'
     h += '<p style="color:navy">Public apps are available to everyone</p>'
     h += '<p style="color:navy">Private apps are not freely available.  You will need a key to access each private app.</p>'
     h += '<p style="color:navy">The icon in the top left corner will always take you back to the main app screen.</p>'
