f3cd8b0d112cb868bbec3122c8b0f3160ccac823
Wade Jakahi <wjakahi@gmail.com>
2016-04-22 12:55:47 -0400

    combined info

--- app/newinfo.js
+++ app/newinfo.js
@@ -20,8 +20,10 @@ false||(function(){ // this little trick will have all variables stay local
     h += '<p><img width="'+(sbmApps.iconWidth*2)+'" src="img/homeicon.png"></p>'
     h += '<h3 style="color:maroon">Share It</h3>'
     h += '<p><a href="http://sbm-it.github.io/apps" target="_blank"><img width="'+(sbmApps.iconWidth)+'" src="img/qrcode.png"></a><br>'
-    h += '<p style="color:navy"></p>'
-    h += '<p style="color:navy"></p>'
+    h += '<h3 style="color:maroon">About</h3>'
+    h += '<p style="color:navy">As part of a new "mobile-first" technology development initiative, <a href="http://stonybrookmedicine.edu/" target="_blank">Stony Brook Medicine</a> is developing an App Store.</p>'
+    h += '<p style="color:navy">We seek to assess the cross-platform use of HTML5 Apps, very much along the same lines of <a href="http://smarthealthit.org/smart-on-fhir/" target="_blank">SMART</a>. Similarly, this is also an effort to make the most of patient-facing informatics applications using the new <a href="https://www.hl7.org/fhir/summary.html" target="_blank">HL7 FHIR</a>.'
+    h += '<p style="color:navy">The <i><b>sbmApps</b></i> framework makes use of another technological innovation, this time in Web Technologies: the emergence of WebApp standards supported by Web Browser manufacturers, as described by <a href="http://www.w3.org/2008/webapps/" target="_blank">W3C\'s WebApps Working Group</a>. This mechanism uses the "add to home screen" feature in your browser to assemble a native-looking App if a properly formated manifest is found (which sbmApps includes already). The same application will also be ported to the App stores of Chrome, Android, Apple and Microsoft.</p>'
     h += '</div>'
 console.log(h);
     sbmApps.render(h)
