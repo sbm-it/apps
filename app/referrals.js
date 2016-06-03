b4edf8eaecd4df530230d69f1a0de329e1413f5e
Wade Jakahi <Wade.jakahi@stonybrook.edu>
2016-06-03 15:24:04 -0400

    Delete referrals.js
    moved to public

--- app/referrals.js
+++
@@ -1,8 +0,0 @@
-console.log('loaded Physician Referrals.js')
-
-sbmApps.insertApp({
-    name:"referrals",
-    description:"Physician Referrals",
-    icon:"https://sbm-it.github.io/referrals/img/patientcare-icon.png",
-    onclick:"https://sbm-it.github.io/referrals/#/referrals-search"
-})
