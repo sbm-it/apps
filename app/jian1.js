0883555a7d87467449722ce3198be43e3becb9dd
Wade Jakahi <Wade.jakahi@stonybrook.edu>
2016-10-07 11:06:21 -0400

    Delete jian1.js
    Test app.  Was removed by Jian

--- app/jian1.js
+++
@@ -1,5 +0,0 @@
-$.getJSON("https://jiansom.github.io/FirstApp/jianManifest.json")
- .then(function(x){
-     sbmApps.insertApp(x)
-     console.log('inserted manifest for Jiang Hong first app jianManifest.json')
-  })
