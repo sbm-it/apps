30243840cd99ef5be2d0c8ec4e8606cb18ce6ad9
Jonas Almeida <jalmeida@mathbiol.org>
2017-03-06 12:29:49 -0500

    NSQIP height 1000px

--- app/nsqipApp.js
+++ app/nsqipApp.js
@@ -2,6 +2,6 @@ console.log('NSQUIP App loaded');
 
 (function(){
     console.log(':-)')
-    var h = '<iframe src="http://uhmc-tableau:8000/views/NSQIPDashboard/NSQIPMonitor?:embed=y&:showShareOptions=true&:display_count=no&:showVizHome=no" width="100%" width="1000px" height="800px" frameBorder="0"></iframe>'
+    var h = '<iframe src="http://uhmc-tableau:8000/views/NSQIPDashboard/NSQIPMonitor?:embed=y&:showShareOptions=true&:display_count=no&:showVizHome=no" width="1000px" height="1000px" frameBorder="0"></iframe>'
     sbmApps.render(h)
 })()
