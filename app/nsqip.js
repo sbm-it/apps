c78ae546293d4ccdb7f46ce63437298135105ff7
Jonas Almeida <jalmeida@mathbiol.org>
2017-03-06 12:06:49 -0500

    NSQIP v1

--- app/nsqip.js
+++ app/nsqip.js
@@ -1,6 +1,6 @@
 sbmApps.insertApp({
     "name":"NSQIP",
     "description":"Stony Brook Medicine NSQUIP report",
-    "icon":"http://sbm-it.github.io/apps/img/nsqip.png",
-    "onclick":"http://sbm-it.github.io/apps/app/nsqipApp.js"
+    "icon":"/apps/img/nsqip.png",
+    "onclick":"/apps/app/nsqipApp.js"
 })
